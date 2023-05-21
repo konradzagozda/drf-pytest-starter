db73b4e4e094c3a16816f01840f9b32b85252d22
Konrad Zagozda <zagozdakonrad@gmail.com>
2023-05-21 08:47:03 +0000

    add authentication views

--- backend/src/config/urls.py
+++ backend/src/config/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
+
+from example_app.views import ProfileView
 
 schema_patterns = [
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
@@ -31,9 +34,42 @@ api_patterns = [
     path("example_app/", include("example_app.urls")),
 ]
 
+accounts_patterns = [
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("password-reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset-complete/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
+    path(
+        "password-change/",
+        auth_views.PasswordChangeView.as_view(),
+        name="password_change",
+    ),
+    path(
+        "password-change-done/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
+    path("profile/", ProfileView.as_view(), name="profile"),
+]
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("django.contrib.auth.urls")),
+    path("accounts/", include(accounts_patterns)),
     path("api/", include(api_patterns)),
     path("", include(schema_patterns)),
 ]
--- backend/src/example_app/views.py
+++ backend/src/example_app/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 from rest_framework import viewsets
 
 from example_app.models import Message
@@ -7,3 +9,12 @@ from example_app.serializers import MessageSerializer
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = "profile.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["username"] = self.request.user.username
+        return context
