36f78abc4a1bd4bf1529bb6ee87f7ad58e0ecfd1
Konrad Zagozda <zagozdakonrad@gmail.com>
2023-05-24 16:40:36 +0000

    try textarea

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,20 @@
-// @ts-nocheck
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import "./App.css";
 
 function MyForm() {
-  const [inputs, setInputs] = useState({});
+  const [textarea, setTextarea] = useState(
+    "The content of a textarea goes in the value attribute",
+  );
 
   const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs((values) => ({ ...values, [name]: value }));
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert(inputs.username + " " + inputs.age);
+    setTextarea(event.target.value);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Enter your name:
-        <input
-          type="text"
-          name="username"
-          value={inputs.username || ""}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Enter your age:
-        <input type="number" name="age" value={inputs.age || ""} onChange={handleChange} />
-      </label>
-      <input type="submit" />
+    <form>
+      <textarea value={textarea} onChange={handleChange} />
     </form>
   );
 }
