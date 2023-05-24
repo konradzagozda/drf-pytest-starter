bb1185cc06807b7c07bf57bab20e539d73510b51
Konrad Zagozda <zagozdakonrad@gmail.com>
2023-05-24 17:59:27 +0000

    add scss

--- frontend/src/Todos.jsx
+++ frontend/src/Todos.jsx
@@ -1,11 +1,12 @@
 import { memo } from "react";
+import styles from "./my-style.module.scss";
 
 /* eslint-disable react/prop-types */
 const Todos = ({ todos }) => {
   console.log("Todos rendered");
   return (
     <>
-      <h2>My Todos</h2>
+      <h2 className={styles.bigblue}>My Todos</h2>
       {todos.map((todo, index) => {
         return <p key={index}>{todo}</p>;
       })}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import Memo from "Memo";
 
 const Home = () => {
+  const myStyle = {
+    color: "red",
+    backgroundColor: "lightblue",
+    padding: "10px",
+    fontFamily: "Sans-Serif",
+  };
+
   return (
     <>
-      <h1>Home</h1>
+      <h1 style={myStyle}>Home</h1>
       <Memo />
     </>
   );
